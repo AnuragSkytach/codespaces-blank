a3504ae2f004502ff472f3e2cf10a1ea27f9757b
AnuragSkytach <anurag@skytechnolabs.com>
2025-07-12 12:40:29 +0000

    update

--- index.js
+++ index.js
@@ -1,1 +1,86 @@
-console.log("Hello");
+const express = require("express");
+const users = require("./MOCK_DATA.json");
+const fs = require("fs");
+const app = express();
+const PORT = 8000;
+
+app.use(express.urlencoded({ extended: false }));
+
+// ROUTES
+app.get("/users", (req, res) => {
+  const html = `
+        <ul>
+            ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+        </ul>
+    `;
+  res.send(html);
+});
+
+// REST API
+app.get("/api/users", (req, res) => {
+  return res.json(users);
+});
+
+app
+  .route("/api/users/:id")
+  .get((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+  })
+  .put((req, res) => {
+    const id = Number(req.params.id);
+    const index = users.findIndex((user) => user.id === id);
+
+    if (index === -1) {
+      return res.status(404).json({ error: "user not found" });
+    }
+
+    users[index] = { id, ...req.body };
+
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users, null, 2), (err) => {
+      if (err) {
+        return res.status(500).json({ error: "Failed to update user" });
+      }
+      return res.json({ status: "User updated", user: users[index] });
+    });
+    // Edit user with id
+    // return res.json({ status: "Pending" });
+  })
+  .delete((req, res) => {
+    const id = Number(req.params.id);
+    const index = users.findIndex((user) => user.id === id);
+
+    if (index === -1) {
+      return res.status(404).json({ error: "user not found" });
+    }
+
+    const deletedUser = users.splice(index, 1);
+
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users, null, 2), (err) => {
+      if (err) {
+        return res.status(500).json({ error: "Failed to delete user" });
+      }
+      return res.json({ status: "User deleted", user: deletedUser[0] });
+    });
+    // delete user with id
+    // return res.json({ status: "Pending" });
+  });
+
+// app.get("/api/users/:id", (req, res) => {
+//   const id = Number(req.params.id);
+//   const user = users.find((user) => user.id === id);
+//   return res.json(user);
+// });
+
+app.post("/api/users", (req, res) => {
+  const body = req.body;
+  console.log("Body ", body);
+  users.push({ id: users.length + 1, ...body });
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+    return res.json({ status: "Success", id: users.length });
+  });
+  // Create user
+});
+
+app.listen(PORT, () => console.log(`Server Started at port: ${PORT}`));
